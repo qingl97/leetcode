c7c9c9a9d9690e4204521f18f57668ebc8fc0fd2
Qing LIANG <qingl97@gmail.com>
2015-04-04 01:48:14 +0200

    correct 165-CompareVersionNumbers

--- src/algorithms/165-CompareVersionNumbers/Solution.c
+++ src/algorithms/165-CompareVersionNumbers/Solution.c
@@ -19,52 +19,37 @@
  #include <stdio.h>
  #include <string.h>
 
-int compareVersion(char *version1, char *version2){
-    /* get next level */
-    while(1){
-        int count1, count2;
-        // char *s1, *s2;
-        int num1, num2;
-        count1 = count2 = 0;
-        while((*(version1+count1) != '.') && (*(version1+count1) !='\0'))
-            count1++;
-        while((*(version2+count2) != '.') && (*(version2+count2) !='\0'))
-            count2++;
-        printf("count1 = %d\n", count1);
-        printf("count2 = %d\n", count2);
-        char s1[count1+1];
-        char s2[count2+1];
-        strncpy(s1, version1, count1);
-        strncpy(s2, version2, count2);
-        s1[count1] = '\0';
-        s2[count2] = '\0';
-        printf("s1 = %s\n", s1);
-        printf("s2 = %s\n", s2);
-        num1 = atoi(s1);
-        num2 = atoi(s2);
-        printf("num1 = %d\n", num1);
-        printf("num2 = %d\n", num2);
-        if(num1>num2)
-            return 1;
-        if(num1<num2)
-            return -1;
-        /* Same version on this level, then check the next level */
-        if(*(version1+count1) == '\0') printf("end1\n");
-        if(*(version2+count2) == '\0') printf("end2\n");
-        // if((*(version1+count1) == '\0') && (*(version2+count2) != '\0'))
-        //     return -1;
-        // if((*(version1+count1) =! '\0') && (*(version2+count2) == '\0'))
-        //     return 1;
-        if((*(version1+count1) == '\0') && (*(version2+count2) == '\0'))
-            return 0;
-        version1 += count1+1;
-        version2 += count2+1;
+int getVersionNum(char *str, int *digits){
+	/* 如果已达结尾，则认为下级版本都为0 */
+    if(*str == '\0'){
+        *digits = 0;
+        return 0;
     }
+    /* 如果该层有给出版本值，则过滤掉非数字字符 */
+    if(*str == '.')
+        str++;
+    int count = 0;
+    while(str[count] >='0' && str[count]<='9')
+        count++;
+    char tmp[count+1];
+    strncpy(tmp, str, count);
+    tmp[count] = '\0';
+    *digits = count;
+    return atoi(tmp);
 }
 
-int main(){
-	char v1[] = "1";
-	char v2[] = "1.1";
-	printf("%d\n", compareVersion(v1, v2));
-	return 0;
+int compareVersion(char *version1, char *version2){
+    int count1, count2;
+    while(1){
+        if(getVersionNum(version1, &count1) > getVersionNum(version2, &count2))
+            return 1;
+        if(getVersionNum(version1, &count1) < getVersionNum(version2, &count2))
+            return -1;
+        /* 从当前比较级的最后一位数字的下一字符开始进入下一级比较，重置指针 */
+        version1 += count1;
+        version2 += count2;
+        /* 如果都已没有下一级可比较，那么不再取版本号直接返回，否则将进入死循环*/
+        if(*version1 == '\0' && *version2 == '\0')
+        	return 0;
+    }
 }
