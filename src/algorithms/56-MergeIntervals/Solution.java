8d9a5484b3f85412ee382e12db48c877a811d066
qingl97 <qingl97@gmail.com>
2015-05-18 12:11:24 +0200

    test git in eclipse

---
+++ src/algorithms/56-MergeIntervals/Solution.java
@@ -0,0 +1,30 @@
+/**
+ * ---------------------------- Problem Description ---------------------------
+ * Write a function that takes an unsigned integer and returns the number of 
+ * ’1' bits it has (also known as the Hamming weight).
+ * 
+ * For example, the 32-bit integer ’11' has binary representation 
+ * 00000000000000000000000000001011, so the function should return 3.
+ * ----------------------------------------------------------------------------
+ * 
+ * @author LIANG Qing
+ * @Date   2015-05-18
+ */
+
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+	/*
+	 * Do a sort of all the start and end points of the intervals and store them
+	 * in an array. Then */
+    public List<Interval> merge(List<Interval> intervals) {
+        
+    }
+}
